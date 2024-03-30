94cb1c46512d622059734cd3c69eaabac6e6b7cf
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-30 17:00:38 +0500

    duh

--- src/Components/LeftNavbar/NavItem.tsx
+++ src/Components/LeftNavbar/NavItem.tsx
@@ -5,7 +5,7 @@ type props = { isActive: boolean, setIsActive: Dispatch<boolean>, image: string 
 const NavItemComponent: React.FC<props> = (props: props) => { 
     return ( 
         <div className={"root-left-navitem" + props.isActive ? " current-category" : ""}>
-            
+              
         </div>
     )
 }
