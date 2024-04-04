588c451f33385536be4824e3ccccc72dc383a81d
pikoUsername <galymzhan.amantaj@gmail.com>
2024-04-04 21:01:33 +0500

    Added selector

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import LoginPage from './Pages/Users/Login/Login';
 import HomePage from './Pages/Home/Home';
 import RegisterPage from './Pages/Users/Register/Register';
 import ResetPasswordPage from './Pages/Users/ResetPassword/ResetPassword';
-
 import ChatSelectPage from './Pages/Payment/Chat_select/Chat_select';
 import WaitingPage from './Pages/Payment/Waiting/Waiting';
 import ContactPage from './Pages/Contact/Contact';
@@ -16,18 +15,29 @@ import UserSecurityPage from './Pages/Users/UserSettings/Categories/Security';
 import { useState } from 'react';
 import GamesPage from './Pages/Market/Games/Games';
 import WalletPage from './Pages/Payment/Wallet/Wallet';
+import SelectorComponent, { OptionType } from './Components/Selector/Selector';
 
 function App() {	
 	const location = useLocation();
 	const excludeNavbarPaths = ['/login', '/register', '/reset_password'];
 	const showNavbar = !excludeNavbarPaths.includes(location.pathname);
 	const [category, setCategory] = useState<string>('')
+	const options: OptionType[] = [
+		// Your options here
+	  ];
 
 	return (
 		<div className='root-content'>
 			{showNavbar && <LeftNavbar Role={UserRoles.Admin} category={category} setCategory={setCategory}/>}
 			{showNavbar && <Navbar Role={UserRoles.Admin} category={category} setCategory={setCategory}/>}
 			<div className='content'>
+				<SelectorComponent 
+				  options={options}
+				  onChange={(newValue, actionMeta) => {
+					console.log(newValue, actionMeta);
+				  }}
+				  width='50%'
+				/>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<LoginPage />} />
--- src/Components/Selector/Selector.tsx
+++ src/Components/Selector/Selector.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Select, { components, OnChangeValue, ActionMeta, OptionProps } from 'react-select';
+import Select, { components, OnChangeValue, ActionMeta, OptionProps, StylesConfig } from 'react-select';
+import "./Selector.css"
 
 export interface OptionType {
   value: string;
@@ -10,6 +11,7 @@ export interface OptionType {
 interface SelectorComponentProps {
   options: OptionType[];
   onChange: (newValue: OnChangeValue<OptionType, false>, actionMeta: ActionMeta<OptionType>) => void;
+  width: string, 
 }
 
 const CustomOption: React.ComponentType<OptionProps<OptionType, false>> = (props) => {
@@ -23,16 +25,40 @@ const CustomOption: React.ComponentType<OptionProps<OptionType, false>> = (props
   );
 };
 
-const SelectorComponent: React.FC<SelectorComponentProps> = ({ options, onChange }) => {
+const customStyles: StylesConfig<OptionType, false> = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: '#252834',
+    boxShadow: state.isFocused ? 'none' : 'none',
+    borderColor: state.isFocused ? '#465166' : '#384153',
+    '&:hover': {
+      borderColor: state.isFocused ? '#2d3545' : '#2d3545',
+    },
+  }),
+  option: (provided, state) => ( { 
+    ...provided, 
+    backgroundColor: "#252834"
+  }), 
+  noOptionsMessage: (provided, state) => ({ 
+    ...provided, 
+    backgroundColor: "#252834",
+    color: "#a8a8a8"
+  })
+};
+
+const SelectorComponent: React.FC<SelectorComponentProps> = ({ options, onChange, width }) => {
   return (
-    <Select<OptionType, false>
-      className="custom-select"
-      classNamePrefix="select"
-      options={options}
-      onChange={onChange}
-      components={{ Option: CustomOption }}
-      getOptionLabel={(option) => option.label} // adhering to the string type requirement
-    />
+    <div className='selector-container' style={{maxWidth: width}}>
+      <Select<OptionType, false>
+        className="general-selector"
+        classNamePrefix="select"
+        options={options}
+        onChange={onChange}
+        components={{ Option: CustomOption }}
+        getOptionLabel={(option) => option.label} // adhering to the string type requirement
+        styles={customStyles}
+      />
+    </div>
   );
 };
 
