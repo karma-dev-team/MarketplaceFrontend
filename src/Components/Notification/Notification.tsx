0fb2c376a6abaa34237ff36aa64caae586909caf
pikoUsername <galymzhan.amantaj@gmail.com>
2024-04-07 10:20:37 +0500

    Small fix

--- src/Components/Notification/Notification.tsx
+++ src/Components/Notification/Notification.tsx
@@ -16,7 +16,7 @@ const NotificationComponent: React.FC<props> = (props: props) => {
                 <div className="notification-header">
                     <img src={asFileUrl(props.image)} alt="avatarimage" className="notification-image"/>
                 </div>
-                <div className="notification-content">
+                <div className="notification-header">
                     <h3 className="notification-titletext">{props.title}</h3>
                     <p className="notification-info">{props.content.slice(0, 100)}</p>
                 </div>
