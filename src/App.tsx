8a30ee1ea774950711d61be6edc7ac1cf6bea5cb
pikoUsername <galymzhan.amantaj@gmail.com>
2024-04-11 02:06:53 +0500

    Added search bar!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import ProductPage from './Pages/Market/Product/Product';
 import ProductCreatePage from './Pages/Market/ProductCreate/ProductCreate';
 import ProductAnalPage from './Pages/Market/ProductUpdate/ProductUpdate';
 import CarouselComponent from './Components/Carousel/Carousel';
+import SearchbarComponent from './Components/Search/Search';
 
 
 function App() {	
@@ -31,19 +32,15 @@ function App() {
 	const excludeNavbarPaths = ['/login', '/register', '/reset_password'];
 	const showNavbar = !excludeNavbarPaths.includes(location.pathname);
 	const [category, setCategory] = useState<string>('')
-
-	const images: { [key: string]: string } = { 
-		"gay": "https://www.copycat.dev/blog/wp-content/uploads/2022/12/rc.png", 
-		"day": "https://www.copycat.dev/blog/wp-content/uploads/2022/12/rc.png"
-	}
+	const [search, setSearch] = useState<string>('')
 
 	return (
 		<div className='root-content'>
 			{showNavbar && <LeftNavbar Role={UserRoles.Admin} category={category} setCategory={setCategory}/>}
 			{showNavbar && <Navbar Role={UserRoles.Admin} category={category} setCategory={setCategory}/>}
 			<div className='content'>
-				<CarouselComponent images={images}/> 
-
+				<SearchbarComponent placeholder='Введите поиск' searchText={search} onChange={setSearch}/> 
+				
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<LoginPage />} />
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,22 +1,26 @@
-import React, { Dispatch, useState } from 'react';
+import React, { CSSProperties, Dispatch, useState } from 'react';
 import './Search.css'; // Assuming you will define styles in this CSS file
 
-type props = { placeholder: string, onChange: Dispatch<string>, searchText: string }
+type props = { placeholder: string, onChange: Dispatch<string>, searchText: string, style?: CSSProperties }
 
-const SearchbarComponent = (props: props) => {
+const SearchbarComponent: React.FC<props> = (props: props) => {
   return (
-    <div className="root-searchbar">
+    <div className="root-searchbar" style={props.style}>
+      <div className='searchbar-input-container'>
+        <input
+          type="text"
+          value={props.searchText}
+          onChange={(e) => props.onChange(e.target.value)}
+          placeholder={props.placeholder}
+          className="input-width" // Apply the class here
+        />
+        {props.searchText.length > 0 ? <button className='searchbar-clear' onClick={() => props.onChange("")}>
+        <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className='searchbar-icon'><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
+        </button> : null} 
+      </div>
       <button className="searchbar-button">
         Найти
-      </button>
-      <input
-        type="text"
-        value={props.searchText}
-        onChange={(e) => props.onChange(e.target.value)}
-        placeholder={props.placeholder}
-        width={19}
-        height={19}
-      />
+      </button> 
     </div>
   );
 };
